a2115253a3a0e1175cc676d178941029657f5954
Laurens de Vries <leh.devries@gmail.com>
2020-04-02 13:50:51 -0400

    added redux-promise, changed actiontypes to constants and added some other middlewares

--- src/index.jsx
+++ src/index.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'; 
 import '../assets/stylesheets/application.scss';
-import { createStore, combineReducers } from 'redux';
-
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import { logger } from 'redux-logger'
+import reduxPromise from 'redux-promise'
 import citiesReducer from './reducers/cities_reducer'
 import activeCityReducer from './reducers/active_city_reducer'
 import App from './components/app'
@@ -13,11 +14,13 @@ const reducers = combineReducers({
   activeCity: activeCityReducer
 });
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const middlewares = composeEnhancers(applyMiddleware(logger, reduxPromise))
 
 const root = document.getElementById('root');
 if (root) {
   ReactDOM.render(
-    <Provider store={createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())}>
+    <Provider store={createStore(reducers, {}, middlewares)}>
       <App />
     </Provider>, root);
 }
